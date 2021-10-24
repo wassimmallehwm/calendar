805142624e49be98730caa46459af78edf424e4c
wassimmallehwm <wassimmalleh.wm@gmail.com>
2021-10-25 00:54:54 +0100

    setup user auth and redirection

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-import MyCalendar from './modules/calendar/MyCalendar';
+import { lazy, Suspense, useState } from 'react';
+import Layout from './components/layout/Layout';
+import { AuthProvider } from './contexts/AuthContext';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import ProtectedRoute from './routes/guards/ProtectedRoute';
+import 'semantic-ui-css/semantic.min.css';
 
+const Login = lazy(() => import('./components/modules/auth/Login'));
+const MyCalendar = lazy(() => import('./components/modules/calendar/MyCalendar'));
+const Loading = () => {
+  return (<h1>Loading</h1>)
+}
 function App() {
+  const [sideBarOpen, setSideBarOpen] = useState(false);
+
+  const closeSidebar = () => {
+    setSideBarOpen(false);
+  }
+  const openSidebar = () => {
+    setSideBarOpen(true);
+  }
+
   return (
-    <div className="App">
-      <MyCalendar/>
-    </div>
+    <AuthProvider>
+      <Router >
+        <Suspense fallback={(<Loading />)}>
+          <Layout >
+            <Switch>
+              <ProtectedRoute exact path='/' component={MyCalendar}/>
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </Layout>
+        </Suspense>
+      </Router>
+    </AuthProvider>
   );
 }
 
