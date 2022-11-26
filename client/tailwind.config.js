dee8d41f6b59f594239542d07f61fe4fd0f5533b
wassimmallehwm <wassimmalleh.wm@gmail.com>
2022-11-27 00:41:42 +0100

    refactor tailwindcss config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class',
   theme: {
     extend: {
+      backgroundImage: theme => ({
+        'homePage': "url('http://localhost:4000/api/settings/cover')"
+      }),
       colors: {
-        primary: {
+        'primary': {
           50: '#e6ecee',
           100: '#b5c6cd',
           200: '#84a1ac',
@@ -12,7 +16,7 @@ module.exports = {
           500: '#084259',
           600: '#073b50'
         },
-        secondary: {
+        'secondary': {
           50: '#f4e6eb',
           100: '#ddb3c2',
           200: '#c6819a',
@@ -20,9 +24,40 @@ module.exports = {
           400: '#981c49',
           500: '#8C0335',
           600: '#7e0330'
-        }  
+        },
+        'success': {
+          50: '#eafaee',
+          100: '#c1f0cc',
+          200: '#98e6aa',
+          300: '#46d366',
+          400: '#28a745',
+          500: '#22903c',
+          600: '#19672b'
+        }
       }
     },
+  },
+  safelist: [
+    {
+      pattern: /bg-(primary|success|secondary|slate)-(50|100|200|300|400|500|600)/,
+      variants: ['lg', 'hover', 'focus', 'lg:hover'],
+    },
+    {
+      pattern: /text-(primary|success|secondary|slate)-(50|100|200|300|400|500|600)/,
+      variants: ['lg', 'hover', 'focus', 'lg:hover'],
+    },
+    {
+      pattern: /border-(primary|success|secondary|slate)-(50|100|200|300|400|500|600)/,
+      variants: ['lg', 'hover', 'focus', 'lg:hover'],
+    },
+    {
+      pattern: /opacity-(50|60|70|80|90)/,
+      variants: ['lg', 'hover', 'focus', 'lg:hover'],
+    },
+  ],
+  variants: {
+    animation: ["motion-safe"],
+    extend: {},
   },
   plugins: [],
 }
